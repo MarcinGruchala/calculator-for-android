938e3809d888b02a4c1d65b7acdc865594d8388a
Marcin Gruchała <margruchala@gmail.com>
2021-03-09 18:18:22 +0100

    Added onClickListeners and screen update fun.

--- calculator/app/src/main/java/com/example/calculator/MainActivity.kt
+++ calculator/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,10 +2,105 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var screen = ""
+
+        btnZero.setOnClickListener(){
+            screen += "0"
+            updateScreen(screen)
+        }
+
+        btnOne.setOnClickListener() {
+            screen += "1"
+            updateScreen(screen)
+        }
+
+        btnTwo.setOnClickListener(){
+            screen += "2"
+            updateScreen(screen)
+        }
+
+        btnThree.setOnClickListener(){
+            screen += "3"
+            updateScreen(screen)
+        }
+
+        btnFour.setOnClickListener(){
+            screen += "4"
+            updateScreen(screen)
+        }
+
+        btnFive.setOnClickListener(){
+            screen += "5"
+            updateScreen(screen)
+        }
+
+        btnSix.setOnClickListener() {
+            screen += "6"
+            updateScreen(screen)
+        }
+
+        btnSeven.setOnClickListener(){
+            screen += "7"
+            updateScreen(screen)
+        }
+
+        btnEight.setOnClickListener(){
+            screen += "8"
+            updateScreen(screen)
+        }
+
+        btnNine.setOnClickListener(){
+            screen += "9"
+            updateScreen(screen)
+        }
+
+        btnDot.setOnClickListener(){
+            screen += "."
+            updateScreen(screen)
+        }
+
+        btnUndo.setOnClickListener(){
+            screen = screen.dropLast(1)
+            updateScreen(screen)
+        }
+
+        btnPlus.setOnClickListener(){
+            screen += "+"
+            updateScreen(screen)
+        }
+
+        btnMinus.setOnClickListener(){
+            screen += "-"
+            updateScreen(screen)
+        }
+
+        btnMultiply.setOnClickListener(){
+            screen += "X"
+            updateScreen(screen)
+        }
+
+        btnDivide.setOnClickListener(){
+            screen += "/"
+            updateScreen(screen)
+        }
+
+        btnEqual.setOnClickListener(){
+            screen += "="
+            updateScreen(screen)
+        }
     }
+
+    private fun updateScreen(screen: String){
+        tvResult.text = screen
+    }
+
 }
