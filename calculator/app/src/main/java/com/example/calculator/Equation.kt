7aadb3329969c7246bde3232b0a585d97f868bf6
Marcin Gruchała <margruchala@gmail.com>
2021-03-11 16:48:02 +0100

    Updated Equation class with decode and solve fun.

--- calculator/app/src/main/java/com/example/calculator/Equation.kt
+++ calculator/app/src/main/java/com/example/calculator/Equation.kt
@@ -2,9 +2,11 @@ package com.example.calculator
 
 class Equation(var notation: String = "") {
 
-//    val numbers = mutableListOf<Number>()
-//    val operation = mutableListOf<Char>()
-//
+    //private val integerElements: List<Char> = listOf('0','1','2','3','4','5','6','7','8','9')
+    private val operationElements: List<Char> = listOf('+','-','/','X', '^','=')
+    val numbers = mutableListOf<Double>()
+    val operations = mutableListOf<Char>()
+
     fun addElement(newElement: String){
         notation += newElement
     }
@@ -12,26 +14,38 @@ class Equation(var notation: String = "") {
     fun removeLastElement(){
         notation = notation.dropLast(1)
     }
-//
-//    fun updateOperations(operation: Char){
-//        //operation.add()
-//    }
-//
-//    fun updateNumber(){
-//        //numbers.add()
-//    }
 
-    fun solve(){
+
+    fun decode(){
         val equationElements: MutableList<Char> = notation.toMutableList()
-        val integerElements: List<Char> = listOf('0','1','2','3','4','5','6','7','8','9')
-        val operationElements: List<Char> = listOf('+','-','/','X', '^')
+        var numberStart = 0 // current number beginning index
+        var numberEnd = 0 // current Operation Index
+        var currentNumber = 0.0
         for (index in equationElements.indices){
             when(equationElements[index]){
-                in integerElements-> {}
-                in operationElements -> {}
-                '(',')' -> {}
-                '=' -> {}
+                in operationElements -> {
+                    numberEnd = index
+                    numbers.add(notation.subSequence(numberStart,numberEnd).toString().toDouble())
+                    operations.add(notation[index])
+                    numberStart = numberEnd + 1
+                }
             }
         }
+    }
+
+    fun solve(): Number {
+        var operationsResult: Double = numbers[0]
+        for (index in operations.indices){
+            when(operations[index]){
+                '+' -> {
+                    operationsResult += numbers[index+1]
+                }
+                '-' -> {
+                    operationsResult -= numbers[index+1]
+                }
+                '=' -> return  operationsResult
+            }
+        }
+        return operationsResult
     }
 }
